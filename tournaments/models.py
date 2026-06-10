e8e54ff2609ef9ba84bd8d4fd29362c96e42e0f1
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2026-06-11 01:35:07 +0330

    feat(tournaments): add initial app and models
    register tournaments app in project settings, create core tournament, team and match models, and generate initial migration

--- tournaments/models.py
+++ tournaments/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Tournament(models.Model):
+    name = models.CharField(max_length=255)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+class Team(models.Model):
+    name = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+
+class StageChoices(models.TextChoices):
+    GROUP = 'Group Stage'
+    ROUND_32 = 'Round of 32'
+    ROUND_16 = 'Round of 16'
+    QUARTER_FINAL = 'Quarter Final'
+    SEMI_FINAL = 'Semi-Final'
+    FINAL = 'Final'
+
+class Match(models.Model):
+    tournament = models.ForeignKey(Tournament, on_delete=models.SET_NULL, null=True)
+    stage = models.CharField(max_length=255, choices=StageChoices.choices)
+    home_team = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True, related_name="home_matches")
+    away_team = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True, related_name="away_matches")
+    home_score = models.IntegerField()
+    away_score = models.IntegerField()
+    date = models.DateField()
