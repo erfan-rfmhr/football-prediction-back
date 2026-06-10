12353d9add57940c866d361d7dc62a732e31f5ca
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2026-06-11 01:01:25 +0330

    feat(auth): add JWT auth, user registration and profile API
    add required third-party dependencies: djoser, djangorestframework-simplejwt and drf-spectacular
    create accounts app with user registration, JWT token handling and user profile views, serializers and URLs
    configure Django settings for custom user model, DRF authentication and API documentation
    update root URLs to include auth routes and swagger docs when debug is active
    make manage.py executable

---
+++ accounts/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+from .views import RegisterView, UserProfileView
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('profile/', UserProfileView.as_view(), name='profile'),
+]
--- config/urls.py
+++ config/urls.py
@@ -1,22 +1,17 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^api/', include('djoser.urls.jwt')),
+    re_path(r'^api/', include('djoser.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path('schema/', SpectacularAPIView.as_view(), name='schema'),
+        path('docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+        path('docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    ]
