21a9d30ca9909ad61ae3b65ebf5d7ddcfbcd77ea
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2026-06-11 15:05:47 +0330

    feat(predictions): update route to empty string

--- apps/predictions/urls.py
+++ apps/predictions/urls.py
@@ -2,6 +2,6 @@ from apps.predictions.api import PredictionViewSet
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register(r'predictions', PredictionViewSet, basename='predictions')
+router.register('', PredictionViewSet, basename='predictions')
 
 urlpatterns = router.urls
