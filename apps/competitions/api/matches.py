22dd4d859005250707f7e3f7264ed37d31cad7cd
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2026-06-11 19:52:56 +0330

    feat(matches): add filtering and ordering
    Add required filter and ordering configurations to enable filtering and sorting of match API responses

--- apps/competitions/api/matches.py
+++ apps/competitions/api/matches.py
@@ -1,10 +1,14 @@
+from django_filters.rest_framework.backends import DjangoFilterBackend
 from apps.competitions.api.filters import MatchFilter
 from apps.competitions.models import Match
 from rest_framework import viewsets
 from apps.competitions.api.serializers import MatchSerializer
-
+from rest_framework.filters import OrderingFilter
 
 class MatchViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Match.objects.all()
     serializer_class = MatchSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_class = MatchFilter
+    ordering_fields = ['start_at']
+    ordering = ['-start_at']
