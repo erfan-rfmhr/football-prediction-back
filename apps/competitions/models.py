528e6b69bdac11aa3da5694f35ad4a8a1559fc08
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2026-06-11 18:37:34 +0330

    refactor(competitions): replace dates with season
    add django migration to remove start/end date fields and add season char field

--- apps/competitions/models.py
+++ apps/competitions/models.py
@@ -2,8 +2,7 @@ from django.db import models
 
 class Tournament(models.Model):
     name = models.CharField(max_length=255)
-    start_date = models.DateField()
-    end_date = models.DateField()
+    season = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
