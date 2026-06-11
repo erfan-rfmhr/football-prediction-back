08af7dac32b6029b5029167d32867e638f8a4dd3
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2026-06-11 04:06:17 +0330

    feat(predictions): add prediction model and point choices
    include a unique constraint to enforce one prediction per user per match, and add the initial django migration

--- predictions/models.py
+++ predictions/models.py
@@ -1,3 +1,20 @@
+from base.models import BaseModel
 from django.db import models
 
-# Create your models here.
+class PointsChoices(models.IntegerChoices):
+    EXACT = 10
+    DIFF = 7
+    WINNER = 5
+    WRONG = 2
+
+class Prediction(BaseModel):
+    user = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
+    match = models.ForeignKey('tournaments.Match', on_delete=models.CASCADE)
+    home_score = models.IntegerField()
+    away_score = models.IntegerField()
+    points = models.IntegerField(default=PointsChoices.WRONG, choices=PointsChoices.choices)
+
+    class Meta:
+        constraints  = [
+            models.UniqueConstraint(fields=['user', 'match'], name='unique_prediction_per_match'),
+        ]
