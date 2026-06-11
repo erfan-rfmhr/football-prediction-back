a0f6183f02c120123bc0773e483638be8b994d89
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2026-06-11 18:45:55 +0330

    refactor(competitions): remove country field from Team model
    add corresponding Django migration for the schema change

--- apps/competitions/models.py
+++ apps/competitions/models.py
@@ -9,7 +9,6 @@ class Tournament(models.Model):
 
 class Team(models.Model):
     name = models.CharField(max_length=255)
-    country = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
