111759029932f5066a60b3f920ad6e9eb3255032
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2026-06-11 18:45:19 +0330

    feat(competitions): add custom admin options for Tournament model

--- apps/competitions/admin.py
+++ apps/competitions/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Tournament, Team, Match
 
-admin.site.register(Tournament)
+@admin.register(Tournament)
+class TournamentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'season')
+    list_filter = ('season',)
+    search_fields = ('name',)
+
+
 admin.site.register(Team)
 admin.site.register(Match)
