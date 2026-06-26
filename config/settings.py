f3bd43d1a6d5bd5fdfe5c617fea4cb8f896046f8
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2026-06-26 17:46:39 +0330

    feat: paginate matches page

--- config/settings.py
+++ config/settings.py
@@ -143,6 +143,8 @@ REST_FRAMEWORK = {
     "DEFAULT_THROTTLE_RATES": {
         "anon": "1000/min",
     },
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    'PAGE_SIZE': 25,
 }
 
 # JWT Configuration
